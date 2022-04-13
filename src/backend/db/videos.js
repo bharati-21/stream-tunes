d7a13b84daacea2f6cc167a707be9c9d5751e4e6
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-14 00:21:26 +0530

    feat: added categories and videos backend db

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -4,18 +4,164 @@
  * */
 
 export const videos = [
-  {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
-  },
-  {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-  },
+	{
+		_id: "xOsZj48vXGA",
+		title: "Roz Roz",
+		description:
+			"Roz Roz from The Yellow Diary and Shilpa Rao, is a beautiful take on how relationships suffer when two people stop communicating with one another amidst the every-day grind called life, but what’s important is that we open our hearts to each other remember the bond underneath. Watch Isha Talwar and Arjun Menon express the changing dynamics of their modern day relationship through picturesque contemporary dance.",
+		creator: "The Yellow Diary",
+		categoryName: "Indie India",
+	},
+	{
+		_id: "PJWemSzExXs",
+		title: "Baarishein",
+		creator: "Anuv Jain",
+		description:
+			"Listen to Baarishein by Anuj Jain,a soulful track, with soft music and strong lyrics which defines pain. This breakup tale tells that although the person who loved has left, memories and love are still the same. There is nothing but only best wishes for the long lost love",
+		categoryName: "Indie India",
+	},
+	{
+		_id: "SmaY7RfBgas",
+		title: "Gul",
+		creator: "Anuv Jain",
+		description:
+			"#GUL 🌷 talks about how we all search for fairytales in our dusty, mystical books but these often don’t come true - and that’s okay, we’ll be okay, cause we have that spark, that ‘jaadoogari’ within us! :D",
+		categoryName: "Indie India",
+	},
+	{
+		_id: "r6F1ve6DOqo",
+		title: "Besharam Si Nazrein",
+		creator: "Abhijeet Srivastava",
+		description:
+			"Stealing glances at one's lover, Beshram Si Nazrein sings of love in visual tones. Painting a forlorn picture of uncontrollable desires in the expressful eyes of the lover, the song enpassions the craving to keep gazing at the lover, finding excuses to do so and searching for the loved one unabashedly. Glances that follow the heart, unmindful of everything else, Abhijeet has once more created a sentimental number making its way into the hearts of his audiences.",
+		categoryName: "Indie India",
+	},
+	{
+		_id: "qLCLvzTGFVM",
+		title: "Besharam Si Nazrein",
+		creator: "The Local Train",
+		description:
+			"Dil mere from the album 'Aalas ka Pedh' talks about the human heart which always roams and runs either behind life, material happiness or finding oneself. Everyting is a game entwined and spread out by life, who is the master controller.",
+		categoryName: "Indie India",
+	},
+	{
+		_id: "mRD0-GxqHVo",
+		title: "Heat Waves",
+		creator: "Glass Anomals",
+		description:
+			"Enjoy the new pop song Heat Waves by Glass Animals, a song that describes the feeling of thinking about someone and missing them late at night.",
+		categoryName: "English Pop",
+	},
+	{
+		_id: "2Vv-BfVoq4g",
+		title: "Perfect",
+		creator: "Ed Sheeran",
+		description:
+			"Perfect by Ed Sheeran is a romantic ballad that celebrates young love. This song perfectly captures the love, dreams, and fantasies one has with their loved one!",
+		categoryName: "English Pop",
+	},
+	{
+		_id: "UVcZfQe-Kw",
+		title: "Levitating",
+		creator: "Dua Lipa",
+		description:
+			"Enjoy the feeling of having fun, meeting someone, and falling in love with Levitating by Dua Lipa. Enjoy the otherworldly, levitating experience of meeting someone at the perfect time!",
+		categoryName: "English Pop",
+	},
+	{
+		_id: "nYh-n7EOtMA",
+		title: "Cheap Thrills",
+		creator: "Sia",
+		description:
+			"Listen to the joyful beat about just enjoying way through life just by dancing. You don't need a lot of money to bring all the joy and fun in your life!",
+		categoryName: "English Pop",
+	},
+	{
+		_id: "0KSOMA3QBU0",
+		title: "Dark Horse",
+		creator: "Katy Perry",
+		description:
+			"#1 Pop song of 2014 featuring Katy Perry and Juicy J., Dark Horse is a song about queen who gets anything she sets her eyes upon.",
+		categoryName: "English Pop",
+	},
+	{
+		_id: "mt9xg0mmt28",
+		title: "Tum Se Hi",
+		creator: "T-Series",
+		description:
+			"Full video of the song 'Tum Se Hi' from the movie 'Jab We Met' featuring Shahid Kapoor and Kareena Kapoor, sung by Mohit Chauhan and composed by Pritam.",
+		categoryName: "Bollywood",
+	},
+	{
+		_id: "cmMiyZaSELo",
+		title: "Khuda Jaane",
+		creator: "YRF",
+		description:
+			"Full video of the song 'Khuda Jaane' from the movie 'Bachna Ae Haseeno' featuring Ranbir Kapoor and Deepika Padukone, sung by KK & Shilpa Rao, and composed by Vishal-Shekhar.",
+		categoryName: "Bollywood",
+	},
+	{
+		_id: "6vKucgAeF_Q",
+		title: "Matargashti",
+		creator: "T-Series",
+		description:
+			"Full video of the song 'Matargashti' from the movie 'Tamasha' featuring Ranbir Kapoor and Deepika Padukone, sung by Mohit Chauhan, and composed by A.R. Rahman.",
+		categoryName: "Bollywood",
+	},
+	{
+		_id: "ufGLdU2xyE4",
+		title: "Gulaabo",
+		creator: "Zee Music Company",
+		description:
+			"Full video of the song 'Gulaabo' from the movie 'Shaandaar' featuring Shahid Kapoor and Alia Bhatt, sung by Vishal Dadlani and Anusha Mani, and composed by Amit Trivdei.",
+		categoryName: "Bollywood",
+	},
+    {
+		_id: "w6AZ52kaWTw",
+		title: "Tum Mile (Love Reprise)",
+		creator: "Sony Music India",
+		description:
+			"Full video of the song 'Tum Mile' from the movie 'Tum Mile' featuring Emraan Hashmi and Soha Ali Khan, sung by Neeraj Sridhar and composed by Pritam.",
+		categoryName: "Bollywood",
+	},
+	{
+		_id: "4D5wFhGr334",
+		title: "EDM Thillana : Ananda Bhairavi",
+		creator: "Indian Raga",
+		description:
+			"Check out this EDM-ised version of Thanjavur Shankar Iyer's thillana in Ananda Bhairavi ragam, performed by Vinod Krishnan, Sushmitha Ravikumar with Vivek Ramanan on mridangam. Music arranged by Mahesh Raghvan.",
+        categoryName: "Indian Classical",
+	},
+    {
+		_id: "qps915jQYlI",
+		title: "Swagatham Krishna",
+		creator: "Indian Raga",
+		description:
+			"Swagatham Krishna- The Dance of Krishna, Oothukadu Venkata Subbiar's composition in raga Mohanam, Tishra Adi talam. This contemporary adaptation of the song, with added elements like harmonies and Double track, was conceptualized by Vocalist Madhu Iyer who is accompanied by Poornima Emani (Flute) and Akshay Anantapadmanabhan (Mridangam, Kanjira, Konnakol).",
+        categoryName: "Indian Classical",
+	},
+    {
+		_id: "zkvNnRjIhPE",
+		title: "Mist of Capricorn- Manavyalakincharadate",
+		creator: "Agam",
+		description:
+			"Mist of Capricorn from Agam's sophomore album 'A Dream To Remember' based on 'Manavyalakincharadate' composed by Saint Thyagaraja (1767 - 1847) in the raaga Nalinakaanti set to Deshadi Taala. This adaptation presents a modern touch to the original composition with a mix of modern and traditional music.",
+        categoryName: "Indian Classical",
+	},
+    {
+		_id: "qT9zKMFobYI",
+		title: "Akhilandeshwari",
+		creator: "Trichur Brothers",
+		description:
+			"Presenting Akhilandeshwari, a composition of Shri Muthuswamy Deekshithar, in the raaga DwijavanthiThalam set to Adi Taala. Watch and listen to the song recording from Live Concert of Trichur Brothers @ Toronto, Canada during April 2013.",
+        categoryName: "Indian Classical",
+	},
+    {
+		_id: "DRTkOpKumMs",
+		title: "Krishnaa Nee Begane",
+		creator: "Mahesh Raghvan",
+		description:
+			"Presenting the song Krishnaa Nee Begane, a composition of Vyaasaraaya (1447-1548) set in the raaga of Yamua Kalyaani. Watch and listen to the modern representation of the song with interpretations of the song from three different artists.",
+        categoryName: "Indian Classical",
+	},
 ];
