c7324835d1f06a73f17f1e61a884df860d0785f7
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-14 00:24:45 +0530

    feat: footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+	return (
+		<footer className="footer">
+			<div className="container px-3 py-1 flex-col flex-align-center flex-justify-center">
+				<p className="text-center flex-row flex-align-center flex-justify-center author">
+					<i class="fa-solid fa-code"></i> by{" "}
+					<a
+						href="https://bharati-21.github.io/"
+						className="primary-color"
+					>
+						Bharati Subramanian
+					</a>
+				</p>
+				<ul className="footer-navlinks list list-inline flex-row flex-align-center flex-justify-between">
+					<li className="list-item">
+						<a
+							href="https://twitter.com/_bhaaratii"
+							className="btn btn-link"
+						>
+							<span className="icon">
+								<i class="fa-brands fa-twitter"></i>
+							</span>
+						</a>
+					</li>
+					<li className="list-item">
+						<a
+							href="https://github.com/bharati-21"
+							className="btn btn-link"
+						>
+							<span className="icon">
+								<i class="fa-brands fa-github"></i>
+							</span>
+						</a>
+					</li>
+					<li className="list-item">
+						<a
+							href="https://www.linkedin.com/in/bharati-subramanian-29734b152/"
+							className="btn btn-link"
+						>
+							<span className="icon">
+								<i class="fa-brands fa-linkedin-in"></i>
+							</span>
+						</a>
+					</li>
+					<li className="list-item">
+						<a
+							href="mailto:bharatisharada@gmail.com"
+							className="btn btn-link"
+						>
+							<span className="icon">
+								<i class="fa-solid fa-envelope"></i>
+							</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</footer>
+	);
+};
+
+export { Footer };
