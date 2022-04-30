014731712e95e637215bbd3962453b98d9d59c32
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-30 20:43:51 +0530

    feat: export contexts from common file

---
+++ src/contexts/index.js
@@ -0,0 +1,3 @@
+export { AuthProvider, useAuth } from './auth-context';
+export { ThemeProvider, useTheme } from './theme-context';
+export { useCategories, CategoriesProvider } from './categories-context';
