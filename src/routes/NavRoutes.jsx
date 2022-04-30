c39fff4a51ab9db489e32e4ff875e95471801f33
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-30 20:46:56 +0530

    feat, refactor: login, signup, routes, prettier format

--- src/routes/NavRoutes.jsx
+++ src/routes/NavRoutes.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import Mockman from 'mockman-js';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import Mockman from "mockman-js";
+import { Routes, Route } from "react-router-dom";
 
-import { Home, Explore } from 'pages';
+import { Home, Explore, Login, Signup } from "pages";
+import { ProtectedRoutes } from "./ProtectedRoutes";
 
 const NavRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/explore" element={<Explore />} />
-            <Route path="/mockman" element={<Mockman />} />
-        </Routes>
-    )
-}
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/explore" element={<Explore />} />
+			<Route path="/mockman" element={<Mockman />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/signup" element={<Signup />} />
+			<Route path="/" element={<ProtectedRoutes />}></Route>
+		</Routes>
+	);
+};
 
 export { NavRoutes };
