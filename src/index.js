7dd5884f547ced7fe951c7d3e4664b93bed985b1
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-30 20:49:31 +0530

    feat, refactor: added auth provider, prettier format

--- src/index.js
+++ src/index.js
@@ -2,22 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from "contexts/theme-context";
-import { CategoriesProvider } from "contexts/categories-context";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, CategoriesProvider, AuthProvider } from "contexts/";
+import Portal from "Portal";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <ThemeProvider>
-                <CategoriesProvider>
-                    <App />
-                </CategoriesProvider>
-            </ThemeProvider>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
+	<React.StrictMode>
+		<BrowserRouter>
+			<AuthProvider>
+				<ThemeProvider>
+					<CategoriesProvider>
+						<Portal />
+						<App />
+					</CategoriesProvider>
+				</ThemeProvider>
+			</AuthProvider>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
