2f59c892c3e2a6d828ed5e4a177e6d62d70b4331
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-01 20:52:16 +0530

    feat: category filter chips list

---
+++ src/components/CategoryFiltersList/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { v4 as uuid } from "uuid";
+
+import { useCategories } from "contexts";
+
+const CategoryFiltersList = () => {
+	const { categories } = useCategories();
+
+	const categoryMapping = [
+		{ _id: uuid(), categoryName: "All" },
+		...categories,
+	].map(({ categoryName, _id }) => (
+		<button className="btn btn-link" key={_id}>
+			<span className="badge badge-primary round-pill px-1 text-sm py-0-25">
+				{categoryName}
+			</span>
+		</button>
+	));
+
+	return (
+		<section className="category-list flex-row flex-align-center flex-justify-start flex-wrap">
+			{categoryMapping}
+		</section>
+	);
+};
+
+export { CategoryFiltersList };
