b5c51f646999a4691d44a8d85e4cabe923603d02
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-01 20:53:54 +0530

    feat: video list and video card

---
+++ src/components/VideosList/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { VideoCard } from "components";
+import { useVideos } from "contexts";
+
+const VideosList = () => {
+	const { videos } = useVideos();
+	console.log(videos);
+
+	const videosMapping = videos.map(({ _id, creator, title }) => (
+		<VideoCard
+			key={_id}
+			videoId={_id}
+			videoCreator={creator}
+			videoTitle={title}
+		/>
+	));
+
+	return <section className="videos-list-container">{videosMapping}</section>;
+};
+
+export { VideosList };
