edd6c8681354f896a34f30c3c3a59e1275e2b92e
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-02 20:03:50 +0530

    feat: watch later page

---
+++ src/pages/WatchLater/index.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "pages/Explore/explore.css";
+
+import { useUserData } from "contexts";
+import { Loader, VideosList } from "components";
+import { Link } from "react-router-dom";
+
+const WatchLater = () => {
+	const {
+		userDataLoading,
+		userDataError: { watchlater: watchlaterError },
+		watchlater,
+	} = useUserData();
+
+	return (
+		<main className="main watch-later-main">
+			{watchlaterError || watchlaterError ? (
+				<h3 className="text-center mx-auto px-3 error-color my-3">
+					Watch later videos could not be loaded. Please try again
+					later.
+				</h3>
+			) : userDataLoading || userDataLoading ? (
+				<Loader />
+			) : (
+				<div className="container flex-col flex-align-center flex-justify-start py-1-5 px-3">
+					{watchlater.length ? (
+						<VideosList videos={watchlater} />
+					) : (
+						<div className="py-3">
+							<h3 className="text-center mx-auto px-3 info-color">
+								There are no videos to be watched later. Explore
+								videos to add to your watch later list!
+							</h3>
+							<Link
+								to="/explore"
+								className="btn btn-primary mx-auto mt-2 px-0-75 text-lg py-0-5 text-center"
+							>
+								Explore now
+							</Link>
+						</div>
+					)}
+				</div>
+			)}
+		</main>
+	);
+};
+
+export { WatchLater };
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { Home } from './Home';
 export { Explore } from './Explore';
 export { Login } from './Auth/Login';
 export { Signup } from './Auth/Signup';
+export { WatchLater } from './WatchLater';
