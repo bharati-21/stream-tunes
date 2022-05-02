71dae1d8e6e6a11b241c0abe14b4adea37c229b3
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-02 16:44:57 +0530

    feat: services to post, delete video from watch later

--- src/services/index.js
+++ src/services/index.js
@@ -3,3 +3,5 @@ export { loginService } from './loginService';
 export { signupService } from './signupService';
 export { getVideos } from './getVideos';
 export { getWatchLaterVideosService } from './getWatchLaterVideosService';
+export { postVideoToWatchLaterService } from './postVideoToWatchLaterService';
+export { deleteVideoFromWatchLaterService } from './deleteVideoFromWatchLaterService';
