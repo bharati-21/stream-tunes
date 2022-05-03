52031feb6bda468ccce2fbb14fb27657a1fde421
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-03 11:11:58 +0530

    refactor, feat: function to get liked and watch later vids

--- src/contexts/user-data-context.js
+++ src/contexts/user-data-context.js
@@ -1,8 +1,9 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
 
 import { userDataReducerFunction } from "reducers";
-import { getWatchLaterVideosService } from "services";
+import { getWatchLaterVideosService, getLikedVideosService } from "services";
 import { useAuth } from "./auth-context";
+
 
 const initialUserData = {
 	userDetails: {},
@@ -11,7 +12,7 @@ const initialUserData = {
 	history: [],
 	playlists: [],
 	userDataLoading: true,
-	userDataError: { watchlater: '', history: '', playlists: '', liked: ''},
+	userDataError: { watchlater: "", history: "", playlists: "", likes: "" },
 };
 
 const UserDataContext = createContext(initialUserData);
@@ -25,35 +26,68 @@ const UserDataProvider = ({ children }) => {
 		initialUserData
 	);
 
+	const callGetWatchLaterVideosService = async () => {
+		try {
+			const {
+				data: { watchlater },
+			} = await getWatchLaterVideosService(authToken);
+			userDataDispatch({
+				type: "SET_WATCH_LATER",
+				payload: { watchlater },
+			});
+			userDataDispatch({
+				type: "SET_LOADER",
+				payload: { loading: false },
+			});
+		} catch (error) {
+			userDataDispatch({
+				type: "SET_ERROR",
+				payload: {
+					error: {
+						watchlater:
+							"Watch later videos could not be fetched. Please try again later.",
+					},
+					loading: false,
+				},
+			});
+		}
+	};
+
+	const callGetLikedVideosService = async () => {
+		try {
+			const {
+				data: { likes },
+			} = await getLikedVideosService(authToken);
+			userDataDispatch({
+				type: "SET_LIKES",
+				payload: { likes },
+			});
+			userDataDispatch({
+				type: "SET_LOADER",
+				payload: { loading: false },
+			});
+		} catch (error) {
+			userDataDispatch({
+				type: "SET_ERROR",
+				payload: {
+					error: {
+						likes: "Liked videos could not be fetched. Please try again later.",
+					},
+					loading: false,
+				},
+			});
+		}
+	};
+
 	useEffect(() => {
 		if (isAuth) {
 			userDataDispatch({
 				type: "SET_LOADER",
 				payload: { loading: true },
 			});
-			(async () => {
-				try {
-					const {
-						data: { watchlater },
-					} = await getWatchLaterVideosService(authToken);
-					userDataDispatch({
-						type: "SET_WATCH_LATER",
-						payload: { watchlater },
-					});
-					userDataDispatch({
-						type: "SET_LOADER",
-						payload: { loading: false },
-					});
-				} catch (error) {
-					userDataDispatch({
-						type: "SET_ERROR",
-						payload: {
-							error: { watchlater: "Watch later videos could not be fetched. Please try again later." },
-							loading: false,
-						},
-					});
-				}
-			})();
+
+			callGetWatchLaterVideosService();
+			callGetLikedVideosService();
 		}
 	}, [isAuth]);
 
