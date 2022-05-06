2f3fbdb7d565991d5f8a76bc879e5ef3d6e3f56c
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-06 20:44:46 +0530

    fix: date added field

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -12,7 +12,7 @@ export const videos = [
 		creator: "The Yellow Diary",
 		categoryName: "Indie India",
 		views: 9439168,
-		dateAdded: new Date("February 4, 2021"),
+		dateAdded: "February 4, 2021",
 	},
 	{
 		_id: "PJWemSzExXs",
@@ -22,7 +22,7 @@ export const videos = [
 			"Listen to Baarishein by Anuj Jain,a soulful track, with soft music and strong lyrics which defines pain. This breakup tale tells that although the person who loved has left, memories and love are still the same. There is nothing but only best wishes for the long lost love",
 		categoryName: "Indie India",
 		views: 33156279,
-		dateAdded: new Date("July 9, 2016"),
+		dateAdded: "July 9, 2016",
 	},
 	{
 		_id: "SmaY7RfBgas",
@@ -32,7 +32,7 @@ export const videos = [
 			"#GUL 🌷 talks about how we all search for fairytales in our dusty, mystical books but these often don’t come true - and that’s okay, we’ll be okay, cause we have that spark, that ‘jaadoogari’ within us! :D",
 		categoryName: "Indie India",
 		views: 5395788,
-		dateAdded: new Date("July 9, 2016"),
+		dateAdded: "July 9, 2016",
 	},
 	{
 		_id: "r6F1ve6DOqo",
@@ -42,7 +42,7 @@ export const videos = [
 			"Stealing glances at one's lover, Beshram Si Nazrein sings of love in visual tones. Painting a forlorn picture of uncontrollable desires in the expressful eyes of the lover, the song enpassions the craving to keep gazing at the lover, finding excuses to do so and searching for the loved one unabashedly. Glances that follow the heart, unmindful of everything else, Abhijeet has once more created a sentimental number making its way into the hearts of his audiences.",
 		categoryName: "Indie India",
 		views: 101972,
-		dateAdded: new Date("October 5, 2021"),
+		dateAdded: "October 5, 2021",
 	},
 	{
 		_id: "qLCLvzTGFVM",
@@ -52,7 +52,7 @@ export const videos = [
 			"Dil mere from the album 'Aalas ka Pedh' talks about the human heart which always roams and runs either behind life, material happiness or finding oneself. Everyting is a game entwined and spread out by life, who is the master controller.",
 		categoryName: "Indie India",
 		views: 22244716,
-		dateAdded: new Date("January 29, 2016"),
+		dateAdded: "January 29, 2016",
 	},
 	{
 		_id: "mRD0-GxqHVo",
@@ -62,7 +62,7 @@ export const videos = [
 			"Enjoy the new pop song Heat Waves by Glass Animals, a song that describes the feeling of thinking about someone and missing them late at night.",
 		categoryName: "English Pop",
 		views: 277907908,
-		dateAdded: new Date("June 30, 2020"),
+		dateAdded: "June 30, 2020",
 	},
 	{
 		_id: "2Vv-BfVoq4g",
@@ -72,7 +72,7 @@ export const videos = [
 			"Perfect by Ed Sheeran is a romantic ballad that celebrates young love. This song perfectly captures the love, dreams, and fantasies one has with their loved one!",
 		categoryName: "English Pop",
 		views: 3150929101,
-		dateAdded: new Date("November 9, 2017"),
+		dateAdded: "November 9, 2017",
 	},
 	{
 		_id: "TUVcZfQe-Kw",
@@ -82,7 +82,7 @@ export const videos = [
 			"Enjoy the feeling of having fun, meeting someone, and falling in love with Levitating by Dua Lipa. Enjoy the otherworldly, levitating experience of meeting someone at the perfect time!",
 		categoryName: "English Pop",
 		views: 520145018,
-		dateAdded: new Date("April 9, 2020"),
+		dateAdded: "April 9, 2020",
 	},
 	{
 		_id: "nYh-n7EOtMA",
@@ -92,7 +92,7 @@ export const videos = [
 			"Listen to the joyful beat about just enjoying way through life just by dancing. You don't need a lot of money to bring all the joy and fun in your life!",
 		categoryName: "English Pop",
 		views: 1722376802,
-		dateAdded: new Date("February 11, 2016"),
+		dateAdded: "February 11, 2016",
 	},
 	{
 		_id: "0KSOMA3QBU0",
@@ -102,7 +102,7 @@ export const videos = [
 			"#1 Pop song of 2014 featuring Katy Perry and Juicy J., Dark Horse is a song about queen who gets anything she sets her eyes upon.",
 		categoryName: "English Pop",
 		views: 1722376802,
-		dateAdded: new Date("February 21, 2014"),
+		dateAdded: "February 21, 2014",
 	},
 	{
 		_id: "mt9xg0mmt28",
@@ -112,7 +112,7 @@ export const videos = [
 			"Full video of the song 'Tum Se Hi' from the movie 'Jab We Met' featuring Shahid Kapoor and Kareena Kapoor, sung by Mohit Chauhan and composed by Pritam.",
 		categoryName: "Bollywood",
 		views: 51860308,
-		dateAdded: new Date("May 30, 2011"),
+		dateAdded: "May 30, 2011",
 	},
 	{
 		_id: "cmMiyZaSELo",
@@ -122,7 +122,7 @@ export const videos = [
 			"Full video of the song 'Khuda Jaane' from the movie 'Bachna Ae Haseeno' featuring Ranbir Kapoor and Deepika Padukone, sung by KK & Shilpa Rao, and composed by Vishal-Shekhar.",
 		categoryName: "Bollywood",
 		views: 112503865,
-		dateAdded: new Date("February 3, 2012"),
+		dateAdded: "February 3, 2012",
 	},
 	{
 		_id: "6vKucgAeF_Q",
@@ -132,7 +132,7 @@ export const videos = [
 			"Full video of the song 'Matargashti' from the movie 'Tamasha' featuring Ranbir Kapoor and Deepika Padukone, sung by Mohit Chauhan, and composed by A.R. Rahman.",
 		categoryName: "Bollywood",
 		views: 82740013,
-		dateAdded: new Date("December 8, 2015"),
+		dateAdded: "December 8, 2015",
 	},
 	{
 		_id: "ufGLdU2xyE4",
@@ -142,7 +142,7 @@ export const videos = [
 			"Full video of the song 'Gulaabo' from the movie 'Shaandaar' featuring Shahid Kapoor and Alia Bhatt, sung by Vishal Dadlani and Anusha Mani, and composed by Amit Trivdei.",
 		categoryName: "Bollywood",
 		views: 34481478,
-		dateAdded: new Date("September 10, 2015"),
+		dateAdded: "September 10, 2015",
 	},
 	{
 		_id: "w6AZ52kaWTw",
@@ -152,7 +152,7 @@ export const videos = [
 			"Full video of the song 'Tum Mile' from the movie 'Tum Mile' featuring Emraan Hashmi and Soha Ali Khan, sung by Neeraj Sridhar and composed by Pritam.",
 		categoryName: "Bollywood",
 		views: 40584390,
-		dateAdded: new Date("April 4, 2014"),
+		dateAdded: "April 4, 2014",
 	},
 	{
 		_id: "4D5wFhGr334",
@@ -162,7 +162,7 @@ export const videos = [
 			"Check out this EDM-ised version of Thanjavur Shankar Iyer's thillana in Ananda Bhairavi ragam, performed by Vinod Krishnan, Sushmitha Ravikumar with Vivek Ramanan on mridangam. Music arranged by Mahesh Raghvan.",
 		categoryName: "Indian Classical",
 		views: 3022674,
-		dateAdded: new Date("October 1, 2016"),
+		dateAdded: "October 1, 2016",
 	},
 	{
 		_id: "qps915jQYlI",
@@ -172,7 +172,7 @@ export const videos = [
 			"Swagatham Krishna- The Dance of Krishna, Oothukadu Venkata Subbiar's composition in raga Mohanam, Tishra Adi talam. This contemporary adaptation of the song, with added elements like harmonies and Double track, was conceptualized by Vocalist Madhu Iyer who is accompanied by Poornima Emani (Flute) and Akshay Anantapadmanabhan (Mridangam, Kanjira, Konnakol).",
 		categoryName: "Indian Classical",
 		views: 963817,
-		dateAdded: new Date("March 27, 2020"),
+		dateAdded: "March 27, 2020",
 	},
 	{
 		_id: "zkvNnRjIhPE",
@@ -182,7 +182,7 @@ export const videos = [
 			"Mist of Capricorn from Agam's sophomore album 'A Dream To Remember' based on 'Manavyalakincharadate' composed by Saint Thyagaraja (1767 - 1847) in the raaga Nalinakaanti set to Deshadi Taala. This adaptation presents a modern touch to the original composition with a mix of modern and traditional music.",
 		categoryName: "Indian Classical",
 		views: 5516994,
-		dateAdded: new Date("December 18, 2017"),
+		dateAdded: "December 18, 2017",
 	},
 	{
 		_id: "qT9zKMFobYI",
@@ -192,7 +192,7 @@ export const videos = [
 			"Presenting Akhilandeshwari, a composition of Shri Muthuswamy Deekshithar, in the raaga DwijavanthiThalam set to Adi Taala. Watch and listen to the song recording from Live Concert of Trichur Brothers @ Toronto, Canada during April 2013.",
 		categoryName: "Indian Classical",
 		views: 237217,
-		dateAdded: new Date("July 12, 2018"),
+		dateAdded: "July 12, 2018",
 	},
 	{
 		_id: "DRTkOpKumMs",
@@ -202,6 +202,6 @@ export const videos = [
 			"Presenting the song Krishnaa Nee Begane, a composition of Vyaasaraaya (1447-1548) set in the raaga of Yamua Kalyaani. Watch and listen to the modern representation of the song with interpretations of the song from three different artists.",
 		categoryName: "Indian Classical",
 		views: 2249234,
-		dateAdded: new Date("August 19, 2015"),
+		dateAdded: "August 19, 2015",
 	},
 ];
