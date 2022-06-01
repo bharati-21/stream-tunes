56eccfcbf497a6923065ab1099c1c93ca44e6a72
Bharati Subramanian <bharatisharada@gmail.com>
2022-06-01 20:30:17 +0530

    feat: infinite scroll to display videos

--- src/components/VideosList/index.jsx
+++ src/components/VideosList/index.jsx
@@ -1,17 +1,39 @@
 import React from "react";
 
 import { VideoCard } from "components";
+import { useInfiniteScroll } from "custom-hooks";
+import InfiniteLoader from "assets/images/infinite-loader.svg";
 
 const VideosList = ({ videos }) => {
+	const { pageNumber, loading, lastElementReference, hasMoreVideos } =
+		useInfiniteScroll(videos);
 
-	const videosMapping = videos.map(video => (
-		<VideoCard
-			key={video._id}
-			video={video}
-		/>
+	const videosToDisply = videos?.slice(0, pageNumber * 5);
+
+	const showLoader = () =>
+		videosToDisply?.length &&
+		hasMoreVideos &&
+		loading &&
+		videosToDisply?.length !== videos?.length;
+
+	const videosMapping = videosToDisply.map((video) => (
+		<VideoCard key={video._id} video={video} />
 	));
 
-	return <section className="videos-list-container">{videosMapping}</section>;
+	return (
+		<section className="videos-list-container">
+			{videosMapping}
+			<div className="infinite-scroll-loader" ref={lastElementReference}>
+				{showLoader() ? (
+					<img
+						src={InfiniteLoader}
+						className="img-responsive"
+						alt="Infinite Loader Icon"
+					/>
+				) : null}
+			</div>
+		</section>
+	);
 };
 
 export { VideosList };
